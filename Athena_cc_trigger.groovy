f73ad9e604337bad665c00435a21c01038d8d943
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-05-10 12:56:54 +0530

    Update Athena_cc_trigger.groovy

--- Athena_cc_trigger.groovy
+++ Athena_cc_trigger.groovy
@@ -24,6 +24,6 @@ job('Athena_cc_trigger') {
                    }
 }
 
-
-Athena.doAddJobToView('Athena_cc_trigger')
+myView = hudson.model.Hudson.instance.getView(viewName)
+myView.doAddJobToView('Athena_cc_trigger')
 jenkins.save()
