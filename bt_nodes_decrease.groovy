56b40ed2d0f989041761528d4ac8a3cb538f6542
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-05-18 09:20:00 +0530

    Update bt_nodes_decrease.groovy

--- bt_nodes_decrease.groovy
+++ bt_nodes_decrease.groovy
@@ -8,6 +8,9 @@ Jenkins jenkins = Jenkins.getInstance()
 matrixJob('bt_nodes_decrease') {
     description('This is an bt_nodes_decrease Job DSL job')
     
+        triggers {
+        cron('0 22 9 18 5') // Run the job at 8:00 AM every day
+    }
    axes {
            axis {
                  name('corpId')
