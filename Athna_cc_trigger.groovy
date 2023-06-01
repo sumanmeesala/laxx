d5a7f1e07ce4a23738c4e292c2b24400c38332af
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-06-01 12:42:44 +0530

    Update Athna_cc_trigger.groovy

--- Athna_cc_trigger.groovy
+++ Athna_cc_trigger.groovy
@@ -6,8 +6,16 @@ import hudson.model.ListView;
 Jenkins jenkins = Jenkins.getInstance()
 
 def viewName = 'Athna'
-jenkins.addView (new ListView(viewName))
+// jenkins.addView (new ListView(viewName))
 
+def existingView = Jenkins.instance.getView(viewName)
+if (existingView == null) {
+    // View doesn't exist, create it
+    def newView = new hudson.model.ListView(viewName)
+    newView.save()
+} else {
+    // View already exists
+}
 
 
 job('Athna_cc_trigger') {
@@ -21,7 +29,7 @@ job('Athna_cc_trigger') {
               downstream('ou_substitution_b_m_athna')
                    }
                    
-myView = hudson.model.Hudson.instance.getView(viewName)
+myView = hudson.model.Hudson.instance.getView(newView)
 myView.doAddJobToView('athna_cc_trigger') 
 jenkins.save()
 }
