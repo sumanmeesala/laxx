8f6ef473104b8eb993c191b2f163aeb28e91ddf9
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-06-01 14:35:36 +0530

    Update Athna_cc_trigger.groovy

--- Athna_cc_trigger.groovy
+++ Athna_cc_trigger.groovy
@@ -22,7 +22,7 @@ job('Athna_cc_trigger') {
 
 
     steps {
-        shell('echo "Im Athna_cc_trigger - 4 modified"')
+        shell('echo "Im Athna_cc_trigger - 5 modified"')
     }
         publishers {
               downstream('ou_substitution_b_m_athna')
